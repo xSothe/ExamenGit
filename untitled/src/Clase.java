f7ead0144298f742f72fd9ac78d5451e98ee4381
unknown <xSothe>
2023-05-29 22:59:43 +0200

    corregido y mejorado

--- untitled/src/Clase.java
+++ untitled/src/Clase.java
@@ -1,19 +1,14 @@
 class Clase {
-    int i;
-    String string = "string1";
+    int entero;
+    String string;
 
     public Clase(int i, String string) {
-        this.i = i;
+        this.entero = i;
         this.string = string;
-    }
-
-    public void modificaObjeto(int nuevo_int, String nuevo_string) {
-        i = nuevo_int;
-        string = nuevo_string;
     }
 
     @Override
     public String toString() {
-        return "Objeto { i=" + i + ", a='" + string + "\'}";
+        return "Objeto { i=" + entero + ", a='" + string + "\'}";
     }
 }
