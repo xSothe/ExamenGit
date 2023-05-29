d201a09f724e3cd8858ac694bdb26a9e3ed70063
unknown <xSothe>
2023-05-29 23:33:17 +0200

    comparando_strings solventando conflicto

--- untitled/src/CompareWithEquals.java
+++ untitled/src/CompareWithEquals.java
@@ -1,6 +1,15 @@
-public class CompareWithEquals {
+class CompareWithEquals {
 
     public static void main(String[] args) {
+        String s1 = "hola";
+        String s2 = s1;
+        String s3 = "hola";
+        String s4 =  new String("hola"); // aunque el IDE sugiere no hacer explicita la instancia,...
+
+        System.out.println(s1 == s2); // true
+        System.out.println(s1 == s3); // true
+        System.out.println(s1 == s4); // false: referencias distintas
+        
         String s1 = "hola";
         String s2 = s1;
         String s3 = "hola";
