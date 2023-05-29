7bcc8ea45754e28e55165a1fe6a6347b77fdb51a
unknown <xSothe>
2023-05-29 23:08:18 +0200

    pasando arrays

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -20,8 +22,37 @@ public class Main {
         System.out.println("\n--- Pasando un objeto y modificando su contenido");
         Clase objeto2 = new Clase(1, "string1");
         modificaContenidoObjeto(objeto2); // pasamos la referencia al objeto
-        System.out.println("Valor del objeto fuera del método: " + objeto2);
 
+        System.out.println("Valor del objeto fuera del método: " + objeto2);
+        System.out.println("\n--- Pasando arrays");
+
+        int[] array1 = new int[5];
+        printArray(array1);
+        array1[0] = 0;  // innecesario ya que inicializa a 0 por defecto
+
+        int[] array2 = array1;  // dos variables apuntando al mismo array
+        int[] array3 = Arrays.copyOf(array1, array1.length); // dos arrays iguales
+
+        IncrementaArray(array1);
+        IncrementaArray(array2);
+        IncrementaArray(array3);
+
+        printArray(array1);
+        printArray(array2);
+        printArray(array3);
+
+    }
+
+    private static void IncrementaArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = array[i] + 1;
+        }
+    }
+
+
+    private static void printArray(int[] array) {
+        Arrays.stream(array).forEach(System.out::print);
+        System.out.println();
     }
 
     private static void modificaVarInt(int a) {
